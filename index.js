0a8df055cc397aebd0d0eb155791a996c63f4a65
Valerie Ellis <valerie.ellis@flatironschool.com>
2024-03-30 21:39:14 -0400

    this was a confusing lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 62
+let num2 = 1
+const multiply = num1*num2
 
-// Write your code here
+const random = 1
+
+let num3 = 9
+let num4 = 5
+const mod = num3 % num4
+
+const max = Math.max(20);
